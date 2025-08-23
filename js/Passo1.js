eb3d19638845e01f53af4bed792de7a15e8a6dfd
Greice Lacerda <greicelacerda@gmail.com>
2025-08-22 23:31:01 -0300

    Com som - Perfeito! 22/08/25 ok

--- js/Passo1.js
+++ js/Passo1.js
@@ -20,18 +20,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const progressText = document.getElementById("quiz-progress-text");
   const parabola1 = document.getElementById("grafico-container");
   const parabola2 = document.getElementById("grafico-container2");
+  const errorSound = new Audio("../sons/Erro.mp3");
+  const clapSound = new Audio("../sons/Aplausos.mp3");
+  const PenaSound = new Audio("../sons/Pena.mp3");
+  const confettiConfig = {
+    particleCount: 300,
+    spread: 70,
+    origin: { y: 0.6 },
+  };
 
-  // Esconde os elementos no início
-  proximaPerguntaBtn.style.display = "none";
-  justificativaDiv.style.display = "none";
-  progressBarDiv.style.display = "none";
-  progressBar.style.display = "none";
-  progressText.style.display = "none";
-  refazerQuizzDiv.style.display = "none";
-  parabola2.style.display = "none";
+  function hideAll() {
+    proximaPerguntaBtn.style.display = "none";
+    justificativaDiv.style.display = "none";
+    proximaEtapaBtn.style.display = "none";
+    refazerQuizzDiv.style.display = "none";
+    parabola2.style.display = "none";
+    parabola1.style.display = "block";
+    progressBarDiv.style.display = "none";
+    progressBar.style.display = "none";
+    progressText.style.display = "none";
+  }
 
-
-  // Função para embaralhar as perguntas
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -39,7 +48,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  //Função para a barra de progresso do Quizz
   function updateProgressBar() {
     const progress = (currentQuestionIndex / questions.length) * 100;
     progressBar.style.width = `${progress}%`;
@@ -48,11 +56,50 @@ document.addEventListener("DOMContentLoaded", function () {
       progressBarDiv.style.display = "block";
       progressBar.style.display = "block";
       progressText.style.display = "block";
-    } // NOVA LINHA}
+    }
   }
 
-  //Função que renderiza as perguntas
+  function finalizarQuiz() {
+    const scorePercentage = (correctAnswersCount / questions.length) * 100;
+    alternativasDiv.innerHTML = "";
+    hideAll();
+
+    if (scorePercentage < 70) {
+      PenaSound.play();
+      justificativaDiv.innerHTML = `
+        <h2 style="color: red;">Que Pena!</h2>
+        <div style="text-align: center; line-height: 1.3; font-size: 22px; margin: 0 20px 0 20px;">Sua porcentagem de acertos foi de ${scorePercentage.toFixed(
+          2
+        )}%. Você precisa refazer o Quizz.</div>`;
+      justificativaDiv.style.display = "block";
+      refazerQuizzDiv.style.display = "block";
+      parabola1.style.display = "block";
+    } else {
+      clapSound.play();
+      justificativaDiv.innerHTML = `
+        <h2 style="color: blue;">Parabéns!</h2>
+        <div style="text-align: center; line-height: 1.3; font-size: 22px; margin: 0 20px 0 20px;">Sua porcentagem de acertos foi de ${scorePercentage.toFixed(
+          2
+        )}%. Prossiga para a próxima etapa.</div>`;
+      justificativaDiv.style.display = "block";
+
+      setTimeout(() => {
+        justificativaDiv.innerHTML = `<h2 style="color: blue;">Agora, você está pronto para o Passo 2!</h2><div style="text-align: center; line-height: 1.3; font-size: 22px; margin: 0 20px 0 20px;">Prossiga para a próxima etapa.</div>`;
+        confetti(confettiConfig);
+        clapSound.play();
+        perguntasDiv.innerHTML = `
+          <h2 style="text-align: center; color:blue; line-height: 1.5; font-size: 22px; margin: 20px auto;">Conclusões Importantes:</h2>
+          <div style="text-align: justify; line-height: 1.5; font-size: 16px; margin: 5px auto;">Observe no gráfico que para n<sub>0</sub> = 1, não são gerados retângulos e para n<sub>0</sub> = 2, é gerado apenas um único retângulo inscrito.
+          Por isso, o caso base nessa exploração é n<sub>0</sub> = 2. Comparando o cálculo da área desse retângulo com o valor obtido substituindo n por 2 na fórmula, verificamos a ocorrência do mesmo valor como resultado e conseguimos validar o caso base. Logo, para n = 2, A(2) é verdadeira.</div>`;
+        proximaEtapaBtn.style.display = "block";
+        parabola1.style.display = "none";
+        parabola2.style.display = "block";
+      }, 6000);
+    }
+  }
+
   function mostrarPergunta() {
+    hideAll();
     if (currentQuestionIndex < questions.length) {
       const currentQuestion = questions[currentQuestionIndex];
       perguntasDiv.innerHTML = currentQuestion.pergunta;
@@ -62,62 +109,9 @@ document.addEventListener("DOMContentLoaded", function () {
             `<button class="alternativa-btn" onclick="verificarResposta(${index})">${alt}</button>`
         )
         .join("");
-
-      justificativaDiv.style.display = "none";
-      proximaPerguntaBtn.style.display = "none";
-      proximaEtapaBtn.style.display = "none";
-      parabola2.style.display = "none";
-      parabola1.style.display = "block";
       attempts = 0;
     } else {
-      // Lógica de finalização do quiz
-      proximaPerguntaBtn.style.display = "none";
-
-      const scorePercentage = (correctAnswersCount / questions.length) * 100;
-      justificativaDiv.innerHTML = `
-    <h2 style="color: blue;">Parabéns!</h2>
-    <div style="text-align: center; line-height: 1.3; font-size: 22px; margin: 0 20px 0 20px;">Sua porcentagem de acertos foi de ${scorePercentage.toFixed(
-      2
-    )}%. Prossiga para a próxima etapa.</div>`;
-      justificativaDiv.style.display = "block";
-
-      if (scorePercentage < 70) {
-        // Pontuação insuficiente: exibir mensagem e botão para refazer o quiz
-        justificativaDiv.innerHTML = `
-    <h2 style="color: red;">Que Pena!</h2>
-    <div style="text-align: center; line-height: 1.3; font-size: 22px; margin: 0 20px 0 20px;">Sua porcentagem de acertos foi de ${scorePercentage.toFixed(
-      2
-    )}%. Você precisa refazer o Quizz.</div>`;
-        justificativaDiv.style.display = "block";
-        refazerQuizzDiv.style.display = "block";
-        progressBarDiv.style.display = "none";
-        progressBar.style.display = "none";
-        progressText.style.display = "none";
-        parabola1.style.display = "block";
-        parabola2.style.display = "none";
-
-        // Adicionar este listener para o botão de refazer
-        refazerBtn.addEventListener("click", refazerQuiz);
-      } else {
-        // Pontuação suficiente: exibir mensagem de porcentagem por 2s, depois a final
-        setTimeout(() => {
-          justificativaDiv.innerHTML = `<h2 style="color: blue;">Agora, você está pronto para o Passo 2!</h2><div style="text-align: center; line-height: 1.3; font-size: 22px; margin: 0 20px 0 20px;">Prossiga para a próxima etapa.</div>`;
-          justificativaDiv.style.display = "block";
-          perguntasDiv.innerHTML = `
-          <h2 style="text-align: center; color:blue; line-height: 1.3; font-size: 22px; margin: 20px auto;">Conclusões Importantes:</h2>
-          <div style="text-align: justify; line-height: 1.3; font-size: 16px; margin: 5px auto;">Observe no gráfico que para n<sub>0</sub> = 1, não são gerados retângulos e para n<sub>0</sub> = 2, é gerado apenas um único retângulo inscrito.
-           Por isso, o caso base nessa exploração é n<sub>0</sub> = 2. Comparando o cálculo da área desse retângulo com o valor obtido substituindo n por 2 na fórmula, verificamos a ocorrência do mesmo valor como resultado e conseguimos validar o caso base. Logo, para n = 2, A(2) é verdadeira.</div>.`;
-          alternativasDiv.innerHTML = "";
-          proximaEtapaBtn.style.display = "block";
-          parabola1.style.display = "none";
-          parabola2.style.display = "block";
-          progressBarDiv.style.display = "none";
-          progressBar.style.display = "none";
-          progressText.style.display = "none";
-          proximaEtapaBtn.querySelector("button").onclick = () =>
-            (window.location.href = "./Passo2.html");
-        }, 8000);
-      }
+      finalizarQuiz();
     }
     updateProgressBar();
   }
@@ -127,91 +121,56 @@ document.addEventListener("DOMContentLoaded", function () {
     correctAnswersCount = 0;
     attempts = 0;
     shuffleArray(questions);
-    proximaEtapaBtn.style.display = "none";
-    refazerQuizzDiv.style.display = "none";
-    progressBarDiv.style.display = "none";
-    progressBar.style.display = "none";
-    progressText.style.display = "none";
-    parabola2.style.display = "none";
-    parabola1.style.display = "block";
     mostrarPergunta();
   }
 
-  // Nova função para lidar exclusivamente com a última pergunta
-  function lidarComUltimaPergunta(isCorrect) {
+  window.verificarResposta = function (index) {
+    const currentQuestion = questions[currentQuestionIndex];
+    const isCorrect = index === currentQuestion.respostaCorretaIndex;
+    const isLastQuestion = currentQuestionIndex === questions.length - 1;
+
+    document.querySelectorAll(".alternativa-btn").forEach((btn) => {
+      btn.disabled = true;
+    });
+
     if (isCorrect) {
       correctAnswersCount++;
       justificativaDiv.innerHTML = `<h2 style="color: blue; margin-top: 1px;">Parabéns! Resposta Correta!</h2><div style="text-align: justify; line-height: 1.3; font-size: 16px; margin: 0 20px 0 20px;">${questions[currentQuestionIndex].justificativa}</div>`;
       justificativaDiv.style.display = "block";
       parabola1.style.display = "none";
       parabola2.style.display = "block";
-    } else {
-      justificativaDiv.innerHTML = `<h2 style="color: red;">Que Pena!</h2><div style="text-align: center; line-height: 1.3; font-size: 20px; margin: 0 20px 0 20px;">Você usou todas as suas tentativas.</div>`;
-      justificativaDiv.style.display = "block";
-      parabola1.style.display = "none";
-      parabola2.style.display = "block";
-    }
+      clapSound.play();
 
-    document.querySelectorAll(".alternativa-btn").forEach((btn) => {
-      btn.disabled = true;
-    });
-
-    // Aguarda um breve momento antes de ir para a tela final
-    setTimeout(() => {
-      currentQuestionIndex++;
-      mostrarPergunta();
-    }, 8000);
-  }
-
-  window.verificarResposta = function (index) {
-    const currentQuestion = questions[currentQuestionIndex];
-    const isLastQuestion = currentQuestionIndex === questions.length - 1;
-
-    if (index === currentQuestion.respostaCorretaIndex) {
-      if (isLastQuestion) {
-        lidarComUltimaPergunta(true);
-        parabola1.style.display = "none";
-        parabola2.style.display = "block";
-      } else {
-        correctAnswersCount++;
-        justificativaDiv.innerHTML = `<h2 style="color: blue; margin-top: 1px;">Parabéns! Resposta Correta!</h2><div style="text-align: justify; line-height: 1.3; font-size: 16px; margin: 0 20px 0 20px;">${questions[currentQuestionIndex].justificativa}</div>`;
-        justificativaDiv.style.display = "block";
-
-        document.querySelectorAll(".alternativa-btn").forEach((btn) => {
-          btn.disabled = true;
-        });
+      if (!isLastQuestion) {
         proximaPerguntaBtn.style.display = "block";
-        parabola2.style.display = "block";
-        parabola1.style.display = "none";
+      } else {
+        setTimeout(finalizarQuiz, 6000);
       }
     } else {
       attempts++;
       if (attempts < 3) {
-        justificativaDiv.innerHTML = `<h2 style="color: red;">Resposta incorreta.</h2>
-        <div style="text-align: center; line-height: 1.3; font-size: 22px; margin: 0 20px 0 20px;">Você tem ${
+        justificativaDiv.innerHTML = `<h2 style="color: red;">Resposta incorreta.</h2><div style="text-align: center; line-height: 1.3; font-size: 22px; margin: 0 20px 0 20px;">Você tem ${
           3 - attempts
-        } tentativa(s) restante(s).</div`;
+        } tentativa(s) restante(s).</div>`;
+        errorSound.play();
         justificativaDiv.style.display = "block";
         setTimeout(() => {
           justificativaDiv.style.display = "none";
           justificativaDiv.innerHTML = "";
-        }, 4000);
-      } else {
-        // Se esgotou as tentativas, verifica se é a última pergunta
-        if (isLastQuestion) {
-          lidarComUltimaPergunta(false);
-        } else {
-          justificativaDiv.innerHTML = `<h2 style="color: red;">Que Pena!</h2><div style="text-align: center; line-height: 1.3; font-size: 20px; margin: 0 20px 0 20px;">Você usou todas as suas tentativas. Responda a próxima questão.</div>`;
-          justificativaDiv.style.display = "block";
-          parabola1.style.display = "none";
-          parabola2.style.display = "block";
-
           document.querySelectorAll(".alternativa-btn").forEach((btn) => {
-            btn.disabled = true;
+            btn.disabled = false;
           });
+        }, 2500);
+      } else {
+        justificativaDiv.innerHTML = `<h2 style="color: red;">Que Pena!</h2><div style="text-align: center; line-height: 1.3; font-size: 20px; margin: 0 20px 0 20px;">Você usou todas as suas tentativas.</div>`;
+        PenaSound.play();
+        justificativaDiv.style.display = "block";
+        parabola1.style.display = "none";
+        parabola2.style.display = "block";
+        if (!isLastQuestion) {
           proximaPerguntaBtn.style.display = "block";
-          parabola1.style.display = "none";
-          parabola2.style.display = "block";
+        } else {
+          setTimeout(finalizarQuiz, 6000);
         }
       }
     }
@@ -224,5 +183,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  if (refazerBtn) {
+    refazerBtn.addEventListener("click", refazerQuiz);
+  }
+
+  // Esconde os elementos no início
+  hideAll();
+
   mostrarPergunta();
+
+  // Adiciona o listener do botão de próxima etapa uma única vez
+  if (proximaEtapaBtn.querySelector("button")) {
+    proximaEtapaBtn.querySelector("button").onclick = () =>
+      (window.location.href = "./Passo2.html");
+  }
 });
